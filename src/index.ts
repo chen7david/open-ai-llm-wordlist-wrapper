f87b6b25979bafd27564b32c121d1d9200fc4d27
David M. Chen <chen7david@me.com>
2023-09-17 13:06:20 -0300

    setup: opan ai prompt

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log('hellow world')
+import { config } from 'dotenv'
+import { OpenAI } from 'openai'
+import { ChatCompletionMessageParam } from 'openai/resources/chat'
+config()
+
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY
+})
+
+const systemRole: ChatCompletionMessageParam = {
+    role: 'system',
+    content: `
+        Generate wordlist: 1. water - 水. 
+        Include all meanings from "context:" if provided, e.g., "fire - 火, 辞职.
+    `
+}
+
+
+async function main() {
+    const completion = await openai.chat.completions.create({
+        messages: [
+            systemRole,
+            { 
+                role: 'user', 
+                content: `
+                    house
+                    car
+                    fire
+                `
+            }
+        ],
+        model: 'gpt-3.5-turbo',
+    });
+
+    console.log(completion.choices);
+}
+  
+// main()
+  
